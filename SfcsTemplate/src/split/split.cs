0613db05351189322e1cffeb8e43955289411a79
The Sharp Ninja <ninja@thesharp.ninja>
2021-12-30 12:25:07 -0600

    Expanded 1 files.

--- SfcsTemplate/src/split/split.cs
+++ SfcsTemplate/src/split/split.cs
@@ -1,6 +1,6 @@
 
 var mdFiles = Directory.GetFiles("*.md");
 
-Console.WriteLine(string.Join(Environment.NewLine, mdFiles))
+Console.WriteLine(string.Join(Environment.NewLine, mdFiles));
 
 Console.WriteLine("Done");
